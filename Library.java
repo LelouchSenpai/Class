4e18d7f93c949793ccad164e6c3dc13f15ecbcbe
LelouchSenpai <168114719+LelouchSenpai@users.noreply.github.com>
2024-05-17 17:57:05 +0330

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Library {
+    private String bookName, libraryMember;
+
+    public void setbookName(String n){
+        bookName=n;
+    }
+
+    public void setlibraryMember(String l){
+        libraryMember=l;
+    }
+}
